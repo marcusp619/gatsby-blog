7be65d45116cc6c9a9b3147734314afdbb8c5baa
marcusp619 <marcusp619@gmail.com>
2018-09-25 21:56:16 -0600

    first commit

---
+++ src/pages/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import header from '../css/header.css';
+
+export default () => (
+  <header>
+    <a className="header--logo" href="#">MP</a>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Blog</a>
+      <a href="#">Work</a>
+      <a href="#">About me</a>
+    </nav>
+  </header>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from './components/Header'
+
+import 'normalize.css'
+
+export default () => (
+  <div>
+    <Header />
+  </div>
+)
